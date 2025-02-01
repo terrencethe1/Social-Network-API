0426f68c00970d82015128acdb4d9b53c3739442
terrencethe1 <terrence.jsr@gmail.com>
2025-02-01 16:23:25 -0600

    add get a single user by id to the user-controller file

--- config/controllers/user-controller.js
+++ config/controllers/user-controller.js
@@ -14,3 +14,21 @@ const userController = {
         res.status(500).json(err);
       });
   },
+
+   // Get a single user by id
+   getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'User does not exist.' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
