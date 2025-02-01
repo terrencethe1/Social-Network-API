13ba6f730f5fff8ffac6011238e6b9ccc4a0c0c9
terrencethe1 <terrence.jsr@gmail.com>
2025-02-01 16:05:10 -0600

    added code to get a single thought by id in the thought-controller file

--- config/controllers/thought-controller.js
+++ config/controllers/thought-controller.js
@@ -14,3 +14,18 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  // Get a single thought by id
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'Thought with this ID does not exist.' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
