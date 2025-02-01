5e8142bec33e27717b60eb6aa1938cb536dc86ac
terrencethe1 <terrence.jsr@gmail.com>
2025-02-01 16:21:29 -0600

    add get users to the user-controller file

---
+++ config/controllers/user-controller.js
@@ -0,0 +1,16 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+  
+    // Get all users
+  getUsers(req, res) {
+    User.find()
+      .select('-__v')
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
