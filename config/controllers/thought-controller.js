7af4fcd79272c20594f78010f5388a63da21b319
terrencethe1 <terrence.jsr@gmail.com>
2025-02-01 16:03:50 -0600

    added code to get all thoughts in the thought-controller file

---
+++ config/controllers/thought-controller.js
@@ -0,0 +1,16 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .sort({ createdAt: -1 })
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
