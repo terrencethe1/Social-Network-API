4c8474c6f240f36beed91e116642e93dc33990b7
terrencethe1 <terrence.jsr@gmail.com>
2025-02-01 16:35:08 -0600

    add code to define Mongoose and Define the shape of the documents within the collection

---
+++ models/User.js
@@ -0,0 +1,40 @@
+// Define Mongoose
+
+const { Schema, model } = require('mongoose');
+
+
+//  Define the shape of the documents within the collection.
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
