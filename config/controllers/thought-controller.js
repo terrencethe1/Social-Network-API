da5d7ba9e9b624a955ae8bdca35e4e3810f15fb0
terrencethe1 <terrence.jsr@gmail.com>
2025-02-01 16:06:27 -0600

    added code to create a thought in the thought-controller

--- config/controllers/thought-controller.js
+++ config/controllers/thought-controller.js
@@ -29,3 +29,26 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'Thought has been created but no user with this id!' });
+        }
+
+        res.json({ message: 'Thought has been created!' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
