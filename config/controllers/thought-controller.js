820c5a227ab431a8bd24316cb3db8dfb4832e42f
terrencethe1 <terrence.jsr@gmail.com>
2025-02-01 16:13:24 -0600

    add code to delete a thought in thought-controller file

--- config/controllers/thought-controller.js
+++ config/controllers/thought-controller.js
@@ -67,3 +67,11 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  // Delete a thought
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'Thought with this ID does not exist.' });
+        }
