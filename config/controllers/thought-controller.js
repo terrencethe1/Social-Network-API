46a9b120ed2962ba7ed98dea8b77479a48ba900a
terrencethe1 <terrence.jsr@gmail.com>
2025-02-01 16:12:05 -0600

    add code to update a thought

--- config/controllers/thought-controller.js
+++ config/controllers/thought-controller.js
@@ -52,3 +52,18 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  // Update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'Thought with this ID does not exist.' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
